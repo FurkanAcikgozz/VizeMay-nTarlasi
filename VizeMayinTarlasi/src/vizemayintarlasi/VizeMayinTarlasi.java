d116fe47fbb7e9cabd2a90aa451f17ab8682c5d2
FurkanAcikgozz <65627616+FurkanAcikgozz@users.noreply.github.com>
2020-05-21 00:02:06 +0300

    Add files via upload

---
+++ VizeMayinTarlasi/src/vizemayintarlasi/VizeMayinTarlasi.java
@@ -0,0 +1,13 @@
+
+package vizemayintarlasi;
+
+public class VizeMayinTarlasi {
+
+    public static void main(String[] args) {
+        
+        frmVizeMayin sw= new frmVizeMayin();
+        sw.setExtendedState(javax.swing.JFrame.MAXIMIZED_BOTH);
+        sw.show();
+    }
+    
+}
